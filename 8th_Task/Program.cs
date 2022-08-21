8699fa72c94af7395dc37c7829bec8aeaed38476
Cy8rax <cybralex@gmail.com>
2022-08-22 00:53:59 +1100

    End

---
+++ 8th_Task/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Введите число для получения в нем четных чисел: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while(count<=number)
+{
+    if(count%2==0)
+    {
+        Console.Write(count);
+        Console.Write(", ");
+        count++;
+    }
+    else
+    {
+        count++;
+    }
+}
