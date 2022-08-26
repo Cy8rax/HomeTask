169cb9a8c1ae940fcafae22cbf74fe104a60c8b3
Cy8rax <cybralex@gmail.com>
2022-08-26 23:10:48 +1100

    10th Task

---
+++ Day_2/Task10/Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int number = Convert.ToInt32(Console.ReadLine());
+// или можно int number = new Random().Next(100,1000);
+
+Console.WriteLine(number);
+int qnumber = (number%100)/10;
+
+Console.WriteLine(qnumber);
