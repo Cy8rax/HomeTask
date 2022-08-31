18d6a19553473a96e8b69b0145103dc10d600ed5
Cy8rax <cybralex@gmail.com>
2022-08-31 12:18:32 +1100

    1st Task

---
+++ Day_4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите два числа для возведения первого в натуральную степень второго: ");
+
+int[] array = new int[2];
+void DataInput(int[] data)
+{
+    for(int i=0;i<2;i++)
+    {
+        data[i]=Convert.ToInt32(Console.ReadLine());
+    }
+}
+DataInput(array);
+
