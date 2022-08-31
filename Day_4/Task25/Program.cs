3877f46c0a7cc24ef4c0cd81850a17f95c761957
Cy8rax <cybralex@gmail.com>
2022-08-31 14:48:55 +1100

    Task25 accomplished

---
+++ Day_4/Task25/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.WriteLine("Введите последовательно число и затем степень в которую желаете его возвести: ");
+
+int[] array=new int[3];
+
+void InPut(int[] array)
+{    
+    for(int j=0;j<2;j++)
+    {
+        array[j]=Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+int Degree(int[] array)
+{
+    array[2]=1;
+    for(int i=1;i<=array[1];i++)
+    {
+        array[2]=array[0]*array[2];
+    }
+    return array[2];
+}
+
+InPut(array);
+Console.Clear();
+Console.WriteLine($"Число "+array[0]+" в степени "+array[1]+" равно "+Degree(array));
