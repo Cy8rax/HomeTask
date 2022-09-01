7e76957e2a10cba3a675035f87cab74030c3ca4b
Cy8rax <cybralex@gmail.com>
2022-09-02 00:08:25 +1100

    Task29 Accomplished!

---
+++ Day_4/Task29/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] array = new int[8];
+
+for(int i=0; i<8;i++)
+{
+    array[i]=new Random().Next(1,1000);
+}
+
+void ArrayPrinter(int[] arr)
+{
+    for(int j=0;j<8;j++)
+    Console.Write(arr[j]+", ");
+}
+
+ArrayPrinter(array);
+Console.WriteLine();
