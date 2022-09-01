47c16f4ab8df65e4d821c938f3a28d0b568ff3c4
Cy8rax <cybralex@gmail.com>
2022-09-01 23:37:59 +1100

    27 Accomplished!

---
+++ Day_4/Task27/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите число для получения суммы его цифр: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+int Summarizer(int num)
+{
+    int sum=0;
+    int length = num.ToString().Length;
+    for(int i=0;i<length;i++)
+    {
+        sum+=num%10;
+        num=num/10;
+    }
+    return sum;
+}
+
+Console.Clear();
+Console.WriteLine($"Сумма цифр числа: "+number+" = "+Summarizer(number));
