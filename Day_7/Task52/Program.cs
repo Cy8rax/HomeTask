049f8b66ee34376db48716855694b87e8f37da9d
Cy8rax <cybralex@gmail.com>
2022-09-13 18:53:44 +1100

    Найти среднее арифметическое столбца двумерного массива

---
+++ Day_7/Task52/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+Console.WriteLine("Введите количество строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[row,column];
+
+void FillArray (int[,] array)
+{
+    for(int i=0;i<array.GetLength(0);i++)
+    {
+        Console.WriteLine();
+        for(int j=0;j<array.GetLength(1);j++)
+        {
+            array[i,j]=new Random().Next(1,10);
+            Console.Write(array[i,j]+"\t");
+        }
+    }
+}
+
+void meanNumber(int[,] array)
+{
+    for(int i=0;i<array.GetLength(0);i++)
+    {
+        double mean=0;
+        for(int j=0;j<array.GetLength(1);j++)
+        {
+            mean = array[j,i]+mean;
+        }
+        Console.Write((mean/(array.GetLength(0)))+"\t");
+    }
+}
+
+FillArray(matrix);
+Console.WriteLine();
+Console.WriteLine("Среднее арифметическое каждого столбца: ");
+meanNumber(matrix);
