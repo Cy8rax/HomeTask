59492fcc698f8926a7d06f690f2dd66c4241332d
Cy8rax <cybralex@gmail.com>
2022-09-16 22:37:52 +1100

    Прямоугольный массив с наименьшей строкой

---
+++ Day_8/Task54/Program.cs
@@ -0,0 +1,62 @@
+﻿//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Random random = new Random();
+int row = random.Next(1,10);
+int column = random.Next(1,10);
+
+
+int[,] matrix=new int[row,column];
+
+void fillarray(int[,] array)
+{
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<column;j++)
+        {
+            array[i,j]=random.Next(1,10);
+            Console.Write(array[i,j]+"\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void sorter(int[,] array)
+{
+    int[,] temp=new int[row,column];
+    for(int i=0;i<row;i++)
+    {
+        Console.WriteLine();
+        for(int j=0;j<column;j++)
+        {
+            for(int k=j+1;k<column;k++)
+            {
+                if(array[i,k]>array[i,j])
+                {
+                    int num=array[i,j];
+                    temp[i,j]=array[i,k];
+                    array[i,j]=array[i,k];
+                    array[i,k]=num;
+
+                }
+            }
+        } 
+    }
+}
+
+void printer(int[,] array)
+{
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<column;j++)
+        {
+            Console.Write(array[i,j]+"\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+fillarray(matrix);
+Console.Write("----------------------");
+sorter(matrix);
+printer(matrix);
---
+++ Day_8/Task56/Program.cs
@@ -0,0 +1,61 @@
+﻿//Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Console.WriteLine("Задайте прямоугольный двумерный массив.");
+
+bool condition = true;
+while(condition==true)
+{
+    Console.Write("Введите количество строк: ");
+    int row=Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите количество столбцов: ");
+    int column=Convert.ToInt32(Console.ReadLine());
+    if(row==column)
+    {
+        Console.Clear();
+        Console.WriteLine("Массив должен быть прямоугольным!");
+        condition=true;
+    }
+    else
+    {
+        condition=false;
+        int[,] matrix=new int[row,column];
+        fillArray(matrix);
+        Console.WriteLine($"Номер строки с наименьшей суммой элементов: {minRow(matrix)} строка." );
+    }
+}
+
+void fillArray(int[,] array)
+{
+    for(int i=0;i<array.GetLength(0);i++)
+    {
+        for(int j=0;j<array.GetLength(1);j++)
+        {
+            array[i,j]=new Random().Next(1,10);
+            Console.Write(array[i,j]+"\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int minRow(int[,] array)
+{
+    int[] minSum=new int[array.GetLength(0)];
+
+    for(int i=0;i<array.GetLength(0);i++)
+    {
+        for(int j=0;j<array.GetLength(1);j++)
+        {
+            minSum[i] += array[i,j];
+        }
+    }
+    int minRowNum=0;
+    for(int p=0;p<array.GetLength(0);p++)
+    {
+        if(minSum[0]>minSum[p])
+        {
+            minSum[0]=minSum[p];
+            minRowNum=p+1;
+        }
+    }
+    return minRowNum;
+}
