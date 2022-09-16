10410c3c12016133a5a9038ef09763c3a1a2a643
Cy8rax <cybralex@gmail.com>
2022-09-17 01:44:52 +1100

    3х мерный массив с неповторяющимися числами и координатами

---
+++ Day_8/Task60/Program.cs
@@ -0,0 +1,42 @@
+﻿//Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+Console.WriteLine("Введите последовательно параметры 3х мерного массива: ");
+int row=int.Parse(Console.ReadLine());
+int column=int.Parse(Console.ReadLine());
+int dimension=int.Parse(Console.ReadLine());
+
+int slots=(row*column*dimension);
+int[,,] matrix=new int[row,column,dimension];
+int[] numbers=new int[slots];
+
+for(int p=0;p<slots;p++)
+{
+    bool noneR=false;
+    while(noneR==false)
+    {
+        numbers[p]=new Random().Next(10,100);
+        noneR=true;
+        for(int u=0;u<slots;u++)
+        {
+            if(p!=u&&numbers[p]==numbers[u])
+            {
+                noneR=false;
+            }
+        }
+    }
+}
+
+int l=0;
+for(int i=0;i<row;i++)
+{
+    for(int j=0;j<column;j++)
+    {
+        for(int k=0;k<dimension;k++)
+        {
+            matrix[i,j,k]=numbers[l];
+            l++;
+            Console.Write($"{matrix[i,j,k]}({i},{j},{k})");
+        }
+        Console.WriteLine();
+    }
+}
