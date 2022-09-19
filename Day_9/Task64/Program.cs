0a4a0d693e341438aa39a1d68c19160271024801
Cy8rax <cybralex@gmail.com>
2022-09-19 21:55:40 +1100

    Рекурсия натуральных чисел от M to N

---
+++ Day_9/Task64/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+
+string natNum(int a, int b)
+{
+    if(a==b) return Convert.ToString(a);
+    return a+", "+ natNum(a+1,b);
+}
+Console.WriteLine(natNum(m,n));
