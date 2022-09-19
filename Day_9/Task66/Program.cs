2e78421335b85b17379ab34814f1121054a6c533
Cy8rax <cybralex@gmail.com>
2022-09-19 22:04:52 +1100

    Рекурсия суммы чисел в промежутке

---
+++ Day_9/Task66/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.WriteLine("Введите число M: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int n=int.Parse(Console.ReadLine());
+
+int sumNum(int a, int b)
+{
+    if(a>b) return 0;
+    return a+sumNum(a+1,b);
+}
+
+Console.WriteLine(sumNum(m,n));
